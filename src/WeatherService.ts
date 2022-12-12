1ca10dd939de56b8b9cb7913e7a5533223c39dec
rafaelsolanomartinez <nemesysolano@gmail.com>
2022-12-12 02:11:08 -0400

    Fixed for nagesh

--- src/WeatherService.ts
+++ src/WeatherService.ts
@@ -1,20 +1,24 @@
 import axios from "axios";
 import { countries } from "./Countries";
 import { WeatherServiceResponse } from "./types";
-const WEATHER_SERVICE_URL = "http://localhost:3000/weather";
+const API_KEY = "e8a1d606625a48088bb235114222906";
+const REQUEST_TEMPLATE = `key=${API_KEY}&q=:CITY&aqi=no`;
+const WEATHER_SERVICE_URL = "https://api.weatherapi.com/v1/current.json";
+const DEFAULT_POST_HEADERS = {"Content-Type": "application/x-www-form-urlencoded"};
 
 const randomInt = (min:number, max:number): number => {
+    let result = 0; //TODO: make result a random value between min and max (inclusive)
     
-    let result = 0; //TODO: Generate a non-negative integer between min and max and assigned to result.
     return (result as number);
 }
 
 export  const fetchRandomLocation =  async() => {
     const index = randomInt(0, countries.length-1);
     const country = countries[index];    
-    const city = //TODO: Encode country.capital such that we can include it in the URL below.;
-    const response = await axios.get<WeatherServiceResponse>(`${WEATHER_SERVICE_URL}/${city}`);    
+    const city = // TODO: Encode country.capital as URI component;   
+    const request =  REQUEST_TEMPLATE.replace(":CITY", city);
+    const response = await axios.post<WeatherServiceResponse>(WEATHER_SERVICE_URL, request, {headers: DEFAULT_POST_HEADERS});    
     const data = response.data;
-    data.current.condition.icon =  data.current.condition.icon;
+    data.current.condition.icon = 'https:' + data.current.condition.icon;
     return data;
 }
